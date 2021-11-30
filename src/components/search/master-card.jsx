120c26e73753662edba5ad61a32297cd27d01ffb
nukuutos <nukuutos@gmail.com>
2021-12-01 05:52:43 +1000

    Add e2e test for star and unstar masters

--- src/components/search/master-card.jsx
+++ src/components/search/master-card.jsx
@@ -1,22 +1,25 @@
-import React from "react";
-import ProfileRating from "../profile/header/profile-rating";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import StarProfile from "../profile/header/star-profile";
-import { useRouter } from "next/router";
-import { useSelector, useDispatch } from "react-redux";
-import { deleteMaster, addMaster } from "../../redux/profile/actions";
-import useAsyncAction from "../../hooks/use-async-action/use-async-action";
-import MasterCardRating from "./master-card-rating";
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { useRouter } from 'next/router';
+import { useSelector, useDispatch } from 'react-redux';
+import StarProfile from '../profile/header/star-profile';
+import ProfileRating from '../profile/header/profile-rating';
+import { deleteMaster, addMaster } from '../../redux/profile/actions';
+import useAsyncAction from '../../hooks/use-async-action/use-async-action';
+import MasterCardRating from './master-card-rating';
 
 const getAvatarPath = (avatar) => {
   const avatarPath = `http://localhost:5000/${avatar}`;
-  const defaultPath = "/svg/default.svg";
+  const defaultPath = '/svg/default.svg';
 
   return avatar ? avatarPath : defaultPath;
 };
 
 const MasterCard = ({ master, className, masterCardRef = null }) => {
-  const [{ masters }, { accessToken, id: profileId }] = useSelector((state) => [state.profile, state.auth]);
+  const [{ masters }, { accessToken, id: profileId }] = useSelector((state) => [
+    state.profile,
+    state.auth,
+  ]);
 
   const { firstName, lastName, rating, placeOfwork, avatar, specialization, _id } = master;
 
@@ -28,7 +31,7 @@ const MasterCard = ({ master, className, masterCardRef = null }) => {
     e.stopPropagation();
 
     const config = {
-      method: "put",
+      method: 'post',
       url: `/profile/${profileId}/favorite/${_id}`,
       accessToken,
     };
@@ -42,7 +45,7 @@ const MasterCard = ({ master, className, masterCardRef = null }) => {
     e.stopPropagation();
 
     const config = {
-      method: "delete",
+      method: 'delete',
       url: `/profile/${profileId}/favorite/${_id}`,
       accessToken,
     };
@@ -53,26 +56,41 @@ const MasterCard = ({ master, className, masterCardRef = null }) => {
   };
 
   return (
-    <div ref={masterCardRef} className={`${className} master-card card`} onClick={() => router.push(`/${_id}`)}>
+    <div
+      ref={masterCardRef}
+      className={`${className} master-card card`}
+      onClick={() => router.push(`/${_id}`)}
+    >
       <div className="master-card__identify">
         <img src={getAvatarPath(avatar)} alt="Profile image" className="master-card__avatar" />
         {rating && <MasterCardRating className="master-card__rating" ratingScore={rating} />}
-        {!rating && <MasterCardRating className="master-card__rating master-card__rating--empty" ratingScore={5} />}
+        {!rating && (
+          <MasterCardRating
+            className="master-card__rating master-card__rating--empty"
+            ratingScore={5}
+          />
+        )}
       </div>
       <div className="master-card__biography">
-        <h1 className="master-card__name">{firstName + " " + lastName}</h1>
+        <h1 className="master-card__name">{`${firstName} ${lastName}`}</h1>
         <h2 className="master-card__specialization">{specialization}</h2>
         <div className="master-card__geoposition mt-3">
           <FontAwesomeIcon className="master-card__map-marker" icon="map-marker-alt" />
           {placeOfwork}
         </div>
       </div>
-      <StarProfile
-        onClick={
-          isLoading ? (e) => e.stopPropagation() : masters.includes(_id) ? onClickDeleteMaster : onClickAddMaster
-        }
-        isStarred={masters.includes(_id)}
-      />
+      {profileId && profileId !== _id && (
+        <StarProfile
+          onClick={
+            isLoading
+              ? (e) => e.stopPropagation()
+              : masters.includes(_id)
+              ? onClickDeleteMaster
+              : onClickAddMaster
+          }
+          isStarred={masters.includes(_id)}
+        />
+      )}
     </div>
   );
 };
