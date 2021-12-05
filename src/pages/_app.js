5099bafe650db9697707d50932bf4f2f21ac685b
nukuutos <nukuutos@gmail.com>
2021-12-05 16:33:34 +1000

    Configure jest, change eslint and jsconfig files
    Changes:
    - add custom .babelrc
    - change config of .eslintrc.json and jsconfig.json
    - add regenerator-runtime because of jest

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
+import 'regenerator-runtime/runtime';
+
 import { wrapper } from '../redux/store';
 
 import '../utils/font-awesome';
 import '../sass/main.scss';
 
-const WrappedApp = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
-};
+const WrappedApp = ({ Component, pageProps }) => <Component {...pageProps} />;
 export default wrapper.withRedux(WrappedApp);
