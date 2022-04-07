b0cd09d52eafdf29059ca2151ad128d0d6387bc3
nukuutos <nukuutos@gmail.com>
2022-04-07 23:03:14 +1000

    Add 'display-place-of-work' function

---
+++ src/components/pages/utils/display-place-of-work.js
@@ -0,0 +1,30 @@
+const translateRoomType = {
+  salon: 'салон',
+  cabinet: 'каб.',
+  apartment: 'кв.',
+};
+
+const getRoomValue = (type, value) => {
+  if (type !== 'salon') return value;
+  return <>&laquo;{value}&raquo;</>;
+};
+
+const displayPlaceOfWork = (placeOfWork) => {
+  if (!placeOfWork) return '';
+  if (typeof placeOfWork === 'string') return placeOfWork;
+
+  const { street, house, building, floor, room } = placeOfWork;
+  let stringToDisplay = `ул. ${street} ${house}, `;
+  if (building) stringToDisplay += `к. ${building}, `;
+
+  const { type, value } = room;
+
+  const roomType = translateRoomType[type];
+  const roomValue = getRoomValue(type, value);
+
+  stringToDisplay += `${floor} этаж, ${roomType} ${roomValue}`;
+
+  return stringToDisplay;
+};
+
+export default displayPlaceOfWork;
