a6d91806437446f47c19915bbb341a47ada9500f
nukuutos <nukuutos@gmail.com>
2022-07-12 10:05:48 +1000

    Filter exceptions on adding day to weekends

--- src/components/pages/timetable/timetable-form/timetable-auto/weekends/use-edit-state.js
+++ src/components/pages/timetable/timetable-form/timetable-auto/weekends/use-edit-state.js
@@ -1,14 +1,27 @@
 import { useSelector } from 'react-redux';
 
+const filterExceptions = (exceptions, weekends) => {
+  const filteredExceptions = { ...exceptions };
+
+  for (const weekend of weekends) {
+    filteredExceptions[weekend] = [];
+  }
+
+  return filteredExceptions;
+};
+
 const useEditState = ({ values, editState, setFieldValue }) => {
   const { update } = useSelector((state) => state.timetable);
-  const { weekends } = values.edit.auto;
+  const { auto, edit } = values;
+  const { exceptions } = auto;
+  const { weekends } = edit.auto;
   const [{ isEditing, element }, setEditState] = editState;
 
   const handleEdit = () => {
+    const filteredExceptions = filterExceptions(exceptions, weekends);
     setFieldValue('auto.weekends', weekends);
+    setFieldValue('auto.exceptions', filteredExceptions);
     setEditState({ isEditing: false, element: { ...editState, weekends: false } });
-    // need to delete exceptions on day that we mark as weekend
   };
 
   const handleCancel = () => {
