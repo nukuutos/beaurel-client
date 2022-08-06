75f761936160a03f9dbf4b320a43f2865d75d6ff
nukuutos <nukuutos@gmail.com>
2022-08-06 23:19:04 +1000

    Disable sign up request in production

--- src/components/pages/sign-up/cases/use-on-submit.js
+++ src/components/pages/sign-up/cases/use-on-submit.js
@@ -1,9 +1,11 @@
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import useAsyncAction from '../../../../hooks/use-async-action/use-async-action';
+import { addAlert } from '../../../../redux/alerts/actions';
 
 const useOnSubmit = ({ phone, setIsVerification, disableProgressBar }) => {
   const { city } = useSelector((state) => state.timezone);
   const [asyncAction, isLoading] = useAsyncAction();
+  const dispatch = useDispatch();
 
   const handleSubmit = async (values) => {
     const phoneValue = `+7${values.phone}`;
@@ -15,12 +17,15 @@ const useOnSubmit = ({ phone, setIsVerification, disableProgressBar }) => {
       accessToken: null,
     };
 
-    const response = await asyncAction(config);
-
-    if (response !== null) {
-      phone.current = values.phone;
-      setIsVerification(true);
-      disableProgressBar();
+    if (process.env.NEXT_PUBLIC_NODE_ENV === 'production') {
+      dispatch(addAlert({ message: 'Регистрация невозможна!' }));
+    } else {
+      const response = await asyncAction(config);
+      if (response !== null) {
+        phone.current = values.phone;
+        setIsVerification(true);
+        disableProgressBar();
+      }
     }
   };
 
