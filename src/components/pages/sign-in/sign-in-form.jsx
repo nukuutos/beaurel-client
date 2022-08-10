9dfb0c2fdd2fa816a2269ac0cf466080cbb6b342
nukuutos <nukuutos@gmail.com>
2022-08-10 20:11:06 +1000

    Add loading spinner on sign in request

--- src/components/pages/sign-in/sign-in-form.jsx
+++ src/components/pages/sign-in/sign-in-form.jsx
@@ -4,55 +4,63 @@ import Input from '../../base/form/input';
 import useOnSubmit from './use-on-submit';
 
 const SignInForm = () => {
-  const [handleSubmit] = useOnSubmit();
+  const [handleSubmit, isLoading] = useOnSubmit();
   const router = useRouter();
 
   const goToSignUp = () => router.push('/sign-up');
   const goToForgotPassword = () => router.push('/forgot-password');
 
   return (
-    <section>
-      <Formik initialValues={{ identificator: '', password: '' }} onSubmit={handleSubmit}>
-        <Form className="sign-in__form">
-          <div className="sign-in__group mt-7">
-            <label className="label" htmlFor="identificator">
-              Телефон, username
-            </label>
-            <Input
-              className="input sign-in__input"
-              name="identificator"
-              id="identificator"
-              type="text"
-            />
-          </div>
+    <>
+      {isLoading && <div className="spinner-with-background" />}
+      <div>
+        <Formik initialValues={{ identificator: '', password: '' }} onSubmit={handleSubmit}>
+          <Form className="sign-in__form">
+            <div className="sign-in__group mt-7">
+              <label className="label" htmlFor="identificator">
+                Телефон, username
+              </label>
+              <Input
+                className="input sign-in__input"
+                name="identificator"
+                id="identificator"
+                type="text"
+              />
+            </div>
 
-          <div className="sign-in__group mt-4">
-            <label className="label" htmlFor="password">
-              Пароль
-            </label>
-            <Input className="input sign-in__input" name="password" id="password" type="password" />
-          </div>
+            <div className="sign-in__group mt-4">
+              <label className="label" htmlFor="password">
+                Пароль
+              </label>
+              <Input
+                className="input sign-in__input"
+                name="password"
+                id="password"
+                type="password"
+              />
+            </div>
 
-          <span onClick={goToForgotPassword} className="sign-in__forget-password mt-1">
-            Забыли пароль?
-          </span>
+            <span onClick={goToForgotPassword} className="sign-in__forget-password mt-1">
+              Забыли пароль?
+            </span>
 
-          <button type="submit" className="btn btn--primary sign-in__btn mt-6">
-            Войти
-          </button>
+            <button type="submit" className="btn btn--primary sign-in__btn mt-6">
+              Войти
+            </button>
 
-          <span className="sign-in__or mt-5">или</span>
+            <span className="sign-in__or mt-5">или</span>
 
-          <button
-            onClick={goToSignUp}
-            className="btn btn--secondary sign-in__btn mt-6 mt-5"
-            type="button"
-          >
-            Зарегестрируйся
-          </button>
-        </Form>
-      </Formik>
-    </section>
+            <button
+              onClick={goToSignUp}
+              className="btn btn--secondary sign-in__btn mt-6 mt-5"
+              type="button"
+            >
+              Зарегестрируйся
+            </button>
+          </Form>
+        </Formik>
+      </div>
+    </>
   );
 };
 
