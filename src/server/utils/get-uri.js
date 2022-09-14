97dab9109b4ef863380eaada3c9ad277c88c09c2
nukuutos <nukuutos@gmail.com>
2022-09-14 22:47:07 +1000

    Update `getURI` function

--- src/server/utils/get-uri.js
+++ src/server/utils/get-uri.js
@@ -1,12 +1,6 @@
-const { DB_USER, DB_PASSWORD, DB_CLUSTER, DB_NAME, DB_AUTH_SOURCE, NEXT_PUBLIC_NODE_ENV } =
-  process.env;
+const { DB_USER, DB_PASSWORD, DB_CLUSTER, DB_NAME, DB_AUTH_SOURCE } = process.env;
 
-const getURI = () => {
-  if (NEXT_PUBLIC_NODE_ENV === 'production') {
-    return `mongodb+srv://${DB_USER}:${DB_PASSWORD}@${DB_CLUSTER}/${DB_NAME}`;
-  }
-
-  return `mongodb://${DB_USER}:${DB_PASSWORD}@${DB_CLUSTER}/${DB_NAME}?authSource=${DB_AUTH_SOURCE}`;
-};
+const getURI = () =>
+  `mongodb://${DB_USER}:${DB_PASSWORD}@${DB_CLUSTER}/${DB_NAME}?authSource=${DB_AUTH_SOURCE}`;
 
 export default getURI;
