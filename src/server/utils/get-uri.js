717ad62ea992999357425ff0fe54a030b0b344a8
nukuutos <nukuutos@gmail.com>
2022-09-16 21:04:57 +1000

    Add `console.log` to debug mongodb uri in production

--- src/server/utils/get-uri.js
+++ src/server/utils/get-uri.js
@@ -1,5 +1,9 @@
 const { DB_USER, DB_PASSWORD, DB_CLUSTER, DB_NAME, DB_AUTH_SOURCE } = process.env;
 
+console.log(
+  `mongodb://${DB_USER}:${DB_PASSWORD}@${DB_CLUSTER}/${DB_NAME}?authSource=${DB_AUTH_SOURCE}`
+);
+
 const getURI = () =>
   `mongodb://${DB_USER}:${DB_PASSWORD}@${DB_CLUSTER}/${DB_NAME}?authSource=${DB_AUTH_SOURCE}`;
 
